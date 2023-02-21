65e295ecfe2c821d3a7b5578e627fbddc35fcf13
Edmund Southgate <edsouthgate@me.com>
2023-02-21 21:36:45 +0000

    Adds DB project

---
+++ RecipezDataBase/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Description] NVARCHAR(50) NULL, 
+    [Password] NVARCHAR(50) NULL
+)
