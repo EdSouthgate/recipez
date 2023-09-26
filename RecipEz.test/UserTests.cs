d84667aace6ea6af022c26833065fb29b189a7d8
Edmund Southgate <edsouthgate@me.com>
2023-09-26 15:10:07 +0100

    Adds validation to User

--- RecipEz.test/UserTests.cs
+++ RecipEz.test/UserTests.cs
@@ -15,7 +15,36 @@ namespace RecipEz.test
             // Act 
             // Assert
             Assert.That(user.FirstName, Is.EqualTo("First"));
+        }
+
+        [Test]
+        public void ShouldErrorWhenFirstNameIsLessThanThreeChars()
+        {
+            // Arrange
+            var ex = Assert.Throws<InvalidOperationException>(() => new User("Fi", "Last"));
+            // Act
+            // Assert
+            Assert.AreEqual("First name must be at least 3 characters", ex.Message);
+        }
+
+        [Test]
+        public void ShouldHaveALastName()
+        {
+            // Arrange
+            User user = new User("First", "Last");
+            // Act 
+            // Assert
             Assert.That(user.LastName, Is.EqualTo("Last"));
+        }
+
+        [Test]
+        public void ShouldErrorWhenLastNameIsLessThanThreeChars()
+        {
+            // Arrange
+            var ex = Assert.Throws<InvalidOperationException>(() => new User("First", "La"));
+            // Act
+            // Assert
+            Assert.AreEqual("Last name must be at least 3 characters", ex.Message);
         }
 
         [Test]
@@ -25,7 +54,17 @@ namespace RecipEz.test
             User user = new User("First", "Last", "firstlast@example.com");
             // Act 
             // Assert
-            Assert.That("firstlast@example.com", Is.EqualTo(user.Email));
+            Assert.That("firstlast@example.com", Is.EqualTo(user.Email.ToString()));
+        }
+
+        [Test]
+        public void ShouldErrorIfEmailIsInvalid()
+        {
+            // Arrange
+            var ex = Assert.Throws<InvalidOperationException>(() => new User("First", "Last", "firstlastexample.com"));
+            // Act
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Email address is not valid"));
         }
 
         [Test]
@@ -60,6 +99,17 @@ namespace RecipEz.test
             user.SetPassword("mysecretpassword");
             // Assert
             Assert.That(user.VerifyPassword("incorrect"), Is.False);
+        }
+
+        [Test]
+        public void ShouldHaveRecipeList()
+        {
+            // Arrange
+            User user = new User("First", "Last", "firstlast@example.com");
+
+            // Act
+            // Assert
+            Assert.That(user.Recipes, Is.Not.Null);
         }
     }
 }
--- recipez/User.cs
+++ recipez/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,9 @@ namespace RecipEz
     {
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
-        public string Email { get; private set; }
+        public MailAddress Email { get; private set; }
         public string Description { get; private set; }
+        public List<Recipe> Recipes { get; private set; }
 
         private SHA256 sha256 { get; set; }
 
@@ -20,6 +22,13 @@ namespace RecipEz
 
         public User(string firstName, string lastName)
         {
+            if (firstName.Count() < 3) { 
+                throw new InvalidOperationException("First name must be at least 3 characters");
+            }
+            if(lastName.Count() < 3)
+            {
+                throw new InvalidOperationException("Last name must be at least 3 characters");
+            }
             FirstName = firstName;
             LastName = lastName;
         }
@@ -27,8 +36,15 @@ namespace RecipEz
         public User(string firstName, string lastName, string emailAddress) {
             FirstName = firstName;
             LastName = lastName;
-            Email = emailAddress;
+            try
+            {
+                Email = new MailAddress(emailAddress);
+            } catch (FormatException)
+            {
+                throw new InvalidOperationException("Email address is not valid");
+            }
             sha256 = SHA256.Create();
+            Recipes = new List<Recipe>();
         }
 
         public void SetDescription(string description)
